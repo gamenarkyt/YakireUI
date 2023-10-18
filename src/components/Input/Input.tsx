8a8d9fdf5be4291f0a4cd100747c2a5519018903
gamenarkyt <gagaritin11@gmail.com>
2023-10-18 21:05:07 +0300

    chnage input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,26 +1,66 @@
-import { FC, ReactNode } from "react";
+import {
+  FC,
+  FormEvent,
+  ReactNode,
+  memo,
+  useDeferredValue,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./Input.module.css";
 import clsx from "clsx";
+import { useDebounce } from "@/components/Input/useDebounce";
 
 interface IProps {
   className?: string;
   placeholder?: string;
   leftComponents?: ReactNode;
   rightComponents?: ReactNode;
+  onSubmitCallback?: (finalString: string) => void;
+  onChangeCallback?: (finalString: string) => void;
 }
 
-export const Input: FC<IProps> = ({
+const Input: FC<IProps> = ({
   className,
   placeholder = "Placeholder",
   leftComponents,
   rightComponents,
+  onSubmitCallback,
+  onChangeCallback,
 }) => {
-  const _class = clsx(styles.root, className);
+  const [inputValue, setInputValue] = useState("");
+  const debouncedInputValue = useDebounce(inputValue, 10 0);
+
+  const onInputChangeHandler = (newValue: string) => {
+    if (onChangeCallback) {
+      onChangeCallback(debouncedInputValue);
+    }
+    setInputValue(newValue);
+  };
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (onSubmitCallback) {
+      onSubmitCallback(inputValue);
+    }
+  };
+  console.log("input rerender");
+
+  const _class = clsx(styles.yuiinput, styles.originalinput, className);
   return (
-    <div className={styles.input}>
-      <div>{leftComponents}</div>
-      <input type="text" placeholder={placeholder} className={_class} />
-      <div>{rightComponents}</div>
-    </div>
+    <form className={styles.yuiinput} onSubmit={onSubmit}>
+      {leftComponents}
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={_class}
+        value={inputValue}
+        onChange={(event) => onInputChangeHandler(event.target.value)}
+      />
+      {rightComponents}
+    </form>
   );
 };
+
+export default Input;
